489b0a9aa9221fe224de62a2701793285319b1ae
CYPRIEN <84139302+Cyprien-Gmd@users.noreply.github.com>
2021-06-10 16:13:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("This is a test");
+
+
+// MENU BURGER
+function openNav() {
+    document.getElementById("myNav").style.height = "100vh";
+  }
+  
+function closeNav() {
+document.getElementById("myNav").style.height = "0vh";
+};
