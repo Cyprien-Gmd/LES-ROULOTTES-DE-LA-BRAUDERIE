da94a514188f9945ec59cb44d1029e8734654771
François Gachenot <82226256+Gach85@users.noreply.github.com>
2021-06-10 16:11:12 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+<div class="contacts">
+    <div class="contact1"><i class="fas fa-map-marker-alt"></i><p>La Brauderie
+    49330 BRISSARTHE</p></div>
+    <div class="contact1"><i class="fas fa-phone-alt"></i><p>06.18.91.39.40</p></div>
+    <div class="contact1"><i class="fas fa-comment-alt"></i><p>roulottes.brauderie@gmail.com</p></div>
+</div>
+<div class="subfooter">
+<span class="ligne"></span>
+<div class="mentions">
+    <p>© 2021 Les roulottes de la Brauderie | Site réalisé par François, Filipe, Cyprien et propulsé par WordPress | Mentions légales</p>
+</div>
+</div>
+</footer>
+
+    
+</body>
+</html>
